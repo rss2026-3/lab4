58ed1951c20601673b2475c30bc93a539bb23d8a
Careen <careen@mit.edu>
2026-03-16 18:18:31 -0400

    made changes to proportional gains

--- visual_servoing/visual_servoing/parking_controller.py
+++ visual_servoing/visual_servoing/parking_controller.py
@@ -1,46 +1,28 @@
 #!/usr/bin/env python3
 
-
 import rclpy
 from rclpy.node import Node
 import numpy as np
-
-
 from vs_msgs.msg import ConeLocation, ParkingError
 from ackermann_msgs.msg import AckermannDriveStamped
-
-
-
-
 class ParkingController(Node):
    """
    A controller for parking in front of a cone.
    Listens for a relative cone location and publishes control commands.
    Can be used in the simulator and on the real robot.
    """
-
-
    def __init__(self):
        super().__init__("parking_controller")
-
-
        self.declare_parameter("drive_topic")
        DRIVE_TOPIC = self.get_parameter("drive_topic").value  # set in launch file; different for simulator vs racecar
-
-
        self.drive_pub = self.create_publisher(AckermannDriveStamped, DRIVE_TOPIC, 10)
        self.error_pub = self.create_publisher(ParkingError, "/parking_error", 10)
 
-
        self.create_subscription(
            ConeLocation, "/relative_cone", self.relative_cone_callback, 1)
-
-
        self.parking_distance = .5  #can be changed 
        self.relative_x = 0
        self.relative_y = 0
-
-
        self.get_logger().info("Parking Controller Initialized")
 
 
@@ -48,11 +30,7 @@ class ParkingController(Node):
        self.relative_x = msg.x_pos
        self.relative_y = msg.y_pos
        drive_cmd = AckermannDriveStamped()
-
-
        #################################
-
-
        # YOUR CODE HERE
        # Use relative position and your control law to set drive_cmd
        drive_cmd.header.stamp = self.get_clock().now().to_msg() 
@@ -60,22 +38,19 @@ class ParkingController(Node):
        angle_to_cone = np.arctan2(self.relative_y, self.relative_x)
        distance_error = distance - self.parking_distance
 
-
        # Steering: proportional to angle toward cone
        Ksteering = 1
        K_speed = 0.5
-       min_speed = 0.2
-
-
        drive_cmd.drive.steering_angle = float(np.clip(Ksteering * angle_to_cone, -0.34, 0.34))
        if abs(angle_to_cone) > np.pi / 3:
-           speed = 0.5 * np.sign(distance_error)    
+           speed = 0.5 * np.sign(distance_error)   
        else:
            speed = K_speed * distance_error
-           if abs(distance_error) <  0.1:
-             speed = 0.0
-           elif abs(speed) < 0.5:
-             speed =0.5 * np.sign(distance_error)   
+           if abs(distance_error) < 0.05:
+               speed = 0.0
+           elif abs(speed) < 0.3:
+               speed = 0.3 * np.sign(distance_error)
+    
        drive_cmd.drive.speed = float(np.clip(speed, -1.0, 1.0))
 
        #################################
